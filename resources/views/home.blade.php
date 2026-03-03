3f7cca2d71ae2d9a150954a476295500d2eff901
Rupesh Dahal <rupeshdahal15@gmail.com>
2026-03-03 17:35:29 +0545

    fix: ensure google adsense account meta tag is correctly placed in home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
+    <meta name="google-adsense-account" content="ca-pub-6697940390340424">
+
     {{-- Favicon --}}
     <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon-32.png') }}">
     <link rel="icon" type="image/png" sizes="192x192" href="{{ asset('logo.png') }}">
